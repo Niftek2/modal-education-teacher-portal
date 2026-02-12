02a7838014709d1468c9801544ec6a22398d333d
Nadia Iftekhar <nadiajiftekhar@gmail.com>
2026-02-12 17:22:45 +0000

    File changes

---
+++ functions/exploreCourseConnection.ts
@@ -0,0 +1,39 @@
+const API_ACCESS_TOKEN = Deno.env.get("THINKIFIC_API_ACCESS_TOKEN");
+
+async function graphQLQuery(query) {
+    const response = await fetch("https://api.thinkific.com/stable/graphql", {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${API_ACCESS_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query })
+    });
+
+    const data = await response.json();
+    if (data.errors) throw new Error(data.errors[0]?.message);
+    return data.data;
+}
+
+Deno.serve(async (req) => {
+    try {
+        // What's in CourseConnection edge.node?
+        const result = await graphQLQuery(`
+            query ExploreCourseConnection {
+                __type(name: "CourseEdge") {
+                    fields {
+                        name
+                        type {
+                            kind
+                            name
+                        }
+                    }
+                }
+            }
+        `);
+        
+        return Response.json(result, { status: 200 });
+    } catch (error) {
+        return Response.json({ error: error.message }, { status: 500 });
+    }
+});
