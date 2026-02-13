2d39453a45957509768fbe10ca7eeab83d886e1e
Nadia Iftekhar <nadiajiftekhar@gmail.com>
2026-02-13 20:02:06 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -51,6 +51,7 @@ import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import Verify from './pages/Verify';
 import WebhookDebug from './pages/WebhookDebug';
+import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
@@ -63,4 +64,5 @@ export const PAGES = {
 export const pagesConfig = {
     mainPage: "Dashboard",
     Pages: PAGES,
+    Layout: __Layout,
 };
